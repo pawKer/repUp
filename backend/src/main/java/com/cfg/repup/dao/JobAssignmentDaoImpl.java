8a4b2c0b310c570c98f04c3c8f3e85df957f1664
Rares Dinu <rares.rck96@yahoo.ro>
2017-07-28 16:55:00 +0100

    Added Handlebars dependency

--- backend/src/main/java/com/cfg/repup/dao/JobAssignmentDaoImpl.java
+++ backend/src/main/java/com/cfg/repup/dao/JobAssignmentDaoImpl.java
@@ -1,10 +1,13 @@
 package com.cfg.repup.dao;
 
 import com.cfg.repup.domain.JobAssignment;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Component;
 
-public class JobAssignmentDaoImpl extends JdbcDaoSupport implements JobAssignmentDao{
+@Component
+public class JobAssignmentDaoImpl implements JobAssignmentDao{
 
     private JdbcTemplate jdbcTemplate;
 

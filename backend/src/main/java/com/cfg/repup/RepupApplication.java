a8ac1ee47726eb976fcf66825cc0239622382ec8
aminecs <m.amine.belabbes@gmail.com>
2017-07-31 14:44:46 +0100

    Enables CORS

--- backend/src/main/java/com/cfg/repup/RepupApplication.java
+++ backend/src/main/java/com/cfg/repup/RepupApplication.java
@@ -2,11 +2,23 @@ package com.cfg.repup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class RepupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RepupApplication.class, args);
+	}
+
+	@EnableWebMvc
+	public class WebConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
 	}
 }
